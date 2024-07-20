e9b6cd7869621cb0ef1e3520685af81d922ff059
762pankaj <762pankajkumar@gmail.com>
2024-07-21 04:34:45 +0530

    Update distance.py

--- distance.py
+++ distance.py
@@ -1,10 +1,3 @@
-"""def sq_dist(t1, t2):
-    x = int(t1[0]-t2[0])
-    y = int(t1[1]-t2[1])
-    z = int(t1[2]-t2[2])
-    square_distance = (x*x+y*y+z*z)
-    return square_distance
-print(int(sq_dist((5, 4, 2), (2, 3, 4))))"""
 def sq_dist(t1, t2):
     square_distance = 0
     for i in range(len(t1)):
